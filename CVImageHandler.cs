24ba941036e9406ced7f3f865aa2a1cfe98e83ca
Sergio <sergio.lacon2@gmail.com>
2022-09-26 13:01:14 +0200

    Initial release

---
+++ CVImageHandler.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace CVSprite_Converter
+{
+    public static class CVImageHandler
+    {
+        /// <summary>
+        /// Generate indexed BMP from given image data.
+        /// </summary>
+        /// <param name="width">Image width.</param>
+        /// <param name="height">Image height.</param>
+        /// <param name="img">Image byte data.</param>
+        /// <param name="palette">Image palette. Always 256 colors.</param>
+        /// <returns>Byte array of generated indexed BMP.</returns>
+        public static byte[] WriteIndexedBMP(int width, int height, byte[] img, byte[] palette)
+        {
+            List<byte> file = new List<byte>();
+
+            //Write BMP header
+            //bmp identifier
+            file.AddRange(Encoding.ASCII.GetBytes("\x42\x04d"));
+            //Image size + Palette size
+            file.AddRange(BitConverter.GetBytes(img.Length + 1078));
+            //Bytes reserved
+            file.AddRange(BitConverter.GetBytes(0));
+            //Palette size
+            file.AddRange(BitConverter.GetBytes(1078));
+            //Always 0x40
+            file.AddRange(BitConverter.GetBytes(40)); 
+            //Image width and height
+            file.AddRange(BitConverter.GetBytes(width));
+            file.AddRange(BitConverter.GetBytes(height));
+            //bits per pixel and pixel related info
+            file.AddRange(BitConverter.GetBytes((short)1));
+            file.AddRange(BitConverter.GetBytes((short)8)); 
+            file.AddRange(BitConverter.GetBytes(0));
+            //Image data size
+            file.AddRange(BitConverter.GetBytes(img.Length));
+            //Reserved
+            file.AddRange(BitConverter.GetBytes(0));
+            file.AddRange(BitConverter.GetBytes(0));
+            file.AddRange(BitConverter.GetBytes(0));
+            file.AddRange(BitConverter.GetBytes(0));
+
+            //Write BMP data
+            //Write palette bytes
+            file.AddRange(palette);
+            //Write image data bytes
+            file.AddRange(img);
+
+            return file.ToArray();
+        }
+
+        /// <summary>
+        /// Generate PNG from an unindexed BMP.
+        /// </summary>
+        /// <param name="palette">Palette to use on this image.</param>
+        /// <param name="inputFile">Unindexed BMP file to read data from.</param>
+        /// <returns>Byte array of converted PNG data.</returns>
+        public static byte[] CreatePNGImage(byte[] palette, string inputFile)
+        {
+            //Start creating indexed BMP
+            byte[] indexed;
+            using (BinaryReader reader = new BinaryReader(File.Open(inputFile, FileMode.Open)))
+            {
+                //Get BMP size, width, height, and image data
+                reader.BaseStream.Seek(2, SeekOrigin.Begin);
+                int size = reader.ReadInt32() - 54;
+                reader.BaseStream.Seek(18, SeekOrigin.Begin);
+                int width = reader.ReadInt32();
+                reader.BaseStream.Seek(22, SeekOrigin.Begin);
+                int height = reader.ReadInt32();
+
+                //Calculate padding of current image
+                int padding = width % 4;
+
+                //Get image data
+                List<byte> data = new List<byte>();
+                reader.BaseStream.Seek(122, SeekOrigin.Begin);
+                int count = 0;
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    //skip unused channels
+                    reader.BaseStream.Seek(2, SeekOrigin.Current);
+                    //read color byte
+                    byte index = reader.ReadByte();
+                    //skip another unused channel
+                    reader.BaseStream.Seek(1, SeekOrigin.Current);
+                    //Add color and add +1 to count for padding calculation
+                    data.Add(index);
+                    count++;
+                    if (count == width && padding != 0)
+                    {
+                        for (int y = 0; y < 4 - padding; ++y) data.Add(0);
+                        count = 0;
+                    }
+                }
+                //Add two 0 bytes to end file
+                data.Add(0);
+                data.Add(0);
+
+                //Generate indexed BMP from current one
+                indexed = WriteIndexedBMP(width, height, data.ToArray(), palette);
+            }
+
+            //Create PNG
+            List<byte> newpng;
+            using (Bitmap indexedBmp = new Bitmap(new MemoryStream(indexed)))
+            {
+                using (MemoryStream png = new MemoryStream())
+                {
+                    indexedBmp.Save(png, ImageFormat.Png);
+                    //test: indexedBmp.Save("test.bmp");
+                    newpng = png.ToArray().ToList();
+                }
+            }
+
+            //Insert tRNS parameter to create transparency index at 0 on palette table
+            //WARNING: Use different index value will require different CRC value
+            byte[] alpha = { 0, 0, 0, 1, 116, 82, 78, 83, 0, 64, 230, 216, 102 };
+            newpng.InsertRange(842, alpha);
+
+            return newpng.ToArray();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CVSprite_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process argument parameters
+            //Show help
+            if (args.Length == 0 || args[0] == "-h")
+                ShowHelp();
+
+            //Load input folder
+            string[] folders = { args[0] };
+            
+            //-m option to load subdirectories
+            if (args.Length >= 4 && args[3] == "-m")
+                folders = Directory.GetDirectories(args[0]);
+            
+            //Do conversion for each folder
+            foreach (string folder in folders)
+            {
+                //Determine main palette and set palette extract mode with -a
+                int pal_num = Int32.Parse(args[1]) - 1;
+                bool all_pal = false;
+
+                if (args.Length >= 3)
+                    all_pal = args[2] == "-a";
+
+                if (!Directory.Exists(args[0]))
+                {
+                    Console.WriteLine("Folder doesn't exist!");
+                    ShowHelp();
+                }
+
+                //List all BMPs of current folder
+                string pal_file = "";
+                List<byte[]> palette = new List<byte[]>();
+                List<string> files = Directory.GetFiles(folder, "*.bmp").ToList();
+
+
+                //Exclude palette.bmp and repeated images from AssetStudio
+                for (int x = 0; x < files.Count; ++x)
+                {
+                    if (files[x].Contains("_palette.bmp") || files[x].Contains(" #"))
+                    {
+                        if (files[x].Contains("_palette.bmp"))
+                            pal_file = files[x];
+                        files.RemoveAt(x);
+                        --x;
+                    }
+                }
+
+                //Start conversion of files
+                Console.WriteLine("Start reading from " + folder +
+                    "\nNumber of files: " + files.Count);
+
+                //Create indexed palette reading bytes of image
+                using (BinaryReader reader = new BinaryReader(File.Open(pal_file, FileMode.Open)))
+                {
+                    //Get image width. Is the number of palettes
+                    reader.BaseStream.Seek(22, SeekOrigin.Begin);
+                    int number = reader.ReadInt32();
+
+                    //Get palettes reading bytes
+                    reader.BaseStream.Seek(122, SeekOrigin.Begin);
+                    for (int x = 0; x < number; ++x)
+                        //4 (n of channels per color) * 256 (n of colors)
+                        palette.Add(reader.ReadBytes(4 * 256));
+                }
+
+                //Check palette is out of bounds
+                if (pal_num > palette.Count - 1 || pal_num < 0)
+                {
+                    Console.WriteLine("Palette color out of bounds!");
+                    ShowHelp();
+                }
+
+                //Create image
+                foreach (string file in files)
+                {
+                    //Get file name and create output folder
+                    Console.WriteLine("Reading file " + Path.GetFileName(file));
+                    Directory.CreateDirectory(folder + "/output/");
+
+                    //Save conversion as png inside output folder
+                    string pngname = Path.GetFileNameWithoutExtension(file) + ".png";
+                    File.WriteAllBytes(folder + "/output/" + pngname,
+                        CVImageHandler.CreatePNGImage(palette[pal_num], file));
+
+                    //Save all palettes using first image as base if mode was enabled
+                    if (all_pal)
+                    {
+                        Directory.CreateDirectory(folder + "/output/Palettes/");
+                        for (int x = 0; x < palette.Count; ++x)
+                        {
+                            //Skip main palette for extract since it's already used
+                            if (x == pal_num)
+                                continue;
+                            File.WriteAllBytes(folder + "/output/Palettes/" + (x + 1) + ".png",
+                                CVImageHandler.CreatePNGImage(palette[x], file));
+                        }
+
+                        //Disable mode since it was already extracted for this folder
+                        all_pal = false;
+                    }
+                }
+                Console.WriteLine("All done. Result saved at " + folder + "\\output\\");
+            }
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("cvsprite_converter \"images_folder_path\" \"palette_number\" [-a] [-m]");
+            Console.WriteLine("\tWrite your character folder path that contains the BMP images and palette. Be sure both are on the same folder.");
+            Console.WriteLine("\tThis only support unindexed BMP images. Use AssetStudio tool to get sprites.");
+            Console.WriteLine("\tOn palette number, write the number of palette to use for conversion. The first index is 1.");
+            Console.WriteLine("\tUse -a to generate a palette folder in output folder, which contains an image for each palette the character has.");
+            Console.WriteLine("\tUse -m to read and extract all sprites from all folders inside the input folder.");
+            System.Environment.Exit(1);
+        }
+    }
+}
